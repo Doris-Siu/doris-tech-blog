bc0e66d2c65afb37e294dcb6e145ad467fca8dbb
Doris-Siu <doris.siu.my@gmail.com>
2023-03-30 19:37:51 +0100

    Setting up Sanity.io

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Homepage() {
+  return (
+    <div>
+        <h1 className="text-4xl">Welcome to Doris's blog.</h1>
+    </div>
+  )
+}
---
+++ sanity.config.ts
@@ -0,0 +1,31 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+// import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+// const devOnlyPlugins = [getStartedPlugin()]
+
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+
+
+export default defineConfig({
+  basePath: "/studio",
+  name: 'Doris_Content_Studio',
+  title: 'Doris_Content_Studio',
+  projectId,
+  dataset,
+
+  // plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+  plugins: [deskTool(), visionTool()],
+
+
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
