a952d1f5965f9b8d794088d84b4e1dea5d1d0ba1
Doris-Siu <doris.siu.my@gmail.com>
2023-04-01 23:31:00 +0100

    Implementing Preview Mode (create lib, pages/api)

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,13 @@
+import { previewData } from "next/headers";
 
 export default function Homepage() {
+  if (previewData()) {
+    return <div> Preview mode</div>;
+  }
+
   return (
     <div>
-        
+      <h1> Not in Preview mode </h1>
     </div>
-  )
+  );
 }
