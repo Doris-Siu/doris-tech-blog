224f605b8e689f50c32f26154f565152c2cc0e89
Doris-Siu <doris.siu.my@gmail.com>
2023-04-05 16:30:58 +0100

    Implementing Line Clamp for Tailwind CSS

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -46,7 +46,7 @@ export default function BlogList({ posts }: Props) {
 
             <div className="mt-5 flex-1">
               <p className="underline text-lg font-bold">{post.title}</p>
-              <p className="text-gray-500">
+              <p className="text-gray-500 line-clamp-2">
                 {post.content[0].children[0].text}
               </p>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/line-clamp")],
+};
